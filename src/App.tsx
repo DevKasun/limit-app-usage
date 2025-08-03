f092058e03bdbfd795756ad1ee2b65b34bf24ecd
Kasun Lakshitha <kasun2014@outlook.com>
2025-08-03 21:47:06 +0530

    Setup extension permissions and background service

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,18 @@ function App() {
   const [message] = createSignal("Welcome to Limit App Usage");
 
   return (
-    <div style={{ padding: "20px", width: "300px" }}>
-      <h1>{message()}</h1>
-      <p>Limit your social media usage with this extension.</p>
+    <div
+      style={{
+        padding: "20px",
+        width: "300px",
+        background: "white",
+        "border-radius": "10px",
+      }}
+    >
+      <h2 style={{ color: "black" }}>{message()}</h2>
+      <p style={{ color: "black" }}>
+        Limit your social media usage with this extension.
+      </p>
     </div>
   );
 }
